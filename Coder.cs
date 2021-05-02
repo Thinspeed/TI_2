e4cc0c44ee8228d77f2af866a2abeb746983b6cd
Roman Kozhemyakin <offmailno5@gmail.com>
2021-05-02 15:23:44 +0300

    Добавьте файлы проекта.

---
+++ Coder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TI2
+{
+	class Coder
+	{
+		int publicKey;
+		int mod;
+
+		public Coder(int publicKey, int mod)
+        {
+			this.publicKey = publicKey;
+			this.mod = mod;
+        }
+
+		public string CodeMessage(string message)
+        {
+			var result = new StringBuilder();
+			foreach (var symbol in message)
+			{
+				result.Append((char)CodeSymble(symbol, publicKey, mod));
+			}
+
+			return result.ToString();
+		}
+
+		int CodeSymble(int a, int pow, int mod)
+		{
+			int result = 1;
+			while (pow != 0)
+			{
+				while (pow % 2 == 0)
+				{
+					pow = pow / 2;
+					a = (a * a) % mod;
+				}
+
+				pow = pow - 1;
+				result = (result * a) % mod;
+			}
+
+			return result;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TI2
+{
+	class Program
+	{
+		bool IsNumberPrime(int number)
+		{
+			for (int i = 2; i < number; i++)
+			{
+				if (number % i == 0)
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		static void Main(string[] args)
+		{
+			int p = int.Parse(Console.ReadLine());
+			int q = int.Parse(Console.ReadLine());
+			var decoder = new Decoder(p, q);
+			var coder = new Coder(decoder.PublicKey, decoder.Mod);
+			string message = "BSUIR";
+			var result = coder.CodeMessage(message);
+			result = decoder.DeCodeMessage(result);
+			Console.WriteLine(result);
+		}
+	}
+}
